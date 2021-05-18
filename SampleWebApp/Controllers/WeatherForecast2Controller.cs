100dcd6a731747268a8303388616f8c914b366d1
Bernd Baumanns <baumannsbernd@gmail.com>
2021-05-18 19:38:50 +0200

    working controller added

---
+++ SampleWebApp/Controllers/WeatherForecast2Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecast2Controller : ControllerBase
+    {
+        [HttpGet]
+        [Route("{something}.{extension?}")]
+        public string Get(string something, string extension = null)
+        {
+            return $"Text: {something} {extension}";
+        }
+    }
+}
