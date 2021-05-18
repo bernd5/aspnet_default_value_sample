a30776994be2c57ca5f47855a2b8a0ae0477573c
Bernd Baumanns <baumannsbernd@gmail.com>
2021-05-18 19:30:00 +0200

    sample project added

---
+++ SampleWebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{something}.{extension=foo}")]
+        public string Get(string something, string extension)
+        {
+            return $"Text: {something} {extension}";
+        }
+    }
+}
